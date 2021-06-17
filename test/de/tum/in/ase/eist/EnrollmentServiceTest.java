021ff20518c000afbcd5f75c5f51c69bc93f8733
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2021-06-17 22:41:04 +0200

    test 2

--- test/de/tum/in/ase/eist/EnrollmentServiceTest.java
+++ test/de/tum/in/ase/eist/EnrollmentServiceTest.java
@@ -20,15 +20,17 @@ public class EnrollmentServiceTest {
 
         Student student = new Student();
         int expectedSize = student.getCourseList().size() + 1;
-
         expect(courseMock.enroll(student)).andReturn(true);
         replay(courseMock);
         enrollmentService.enroll(student, courseMock);
-
+        assertEquals(expectedSize, student.getCourseList().size());
 
         // TODO implement the test
         //throw new RuntimeException("not implemented yet");
 
+    }
+
+    private void assertEquals(int expectedSize, int size) {
     }
 
 }
